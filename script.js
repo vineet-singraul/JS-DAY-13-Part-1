23970dfaa75ff7257bd1315b15f214b8105bacc1
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-24 17:32:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var divOfImage = document.getElementById("divOfImage");
+var img = divOfImage.querySelector("img");
+
+let change = () => {
+    img.src = 'https://media.istockphoto.com/id/1265024528/photo/no-better-adventure-buddy.webp?a=1&b=1&s=612x612&w=0&k=20&c=tStWgNSFBAGPyu4gfJfDEjqMPDnvgqWUkIPyZYGS090=';
+};
+
+divOfImage = change;
+
+notchange = () => {
+    img.src = 'https://media.istockphoto.com/id/1127245421/photo/woman-hands-praying-for-blessing-from-god-on-sunset-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=Fbysb-YHuALb_Jyi4Q8Lw2QEVIFa3WSaPsvneGr7Q_8='
+};
+
+
+
+
+
+
+
+
+
+
+divOfImage = notchange;
